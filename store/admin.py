d39d410e10f0d66a74fcc0d3936ca97233a2ea6c
Jawahar28 <jawaharn246@gmail.com>
2025-12-24 19:10:22 +0530

    Chnages made

--- store/admin.py
+++ store/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from . import models
 from django.db.models import Count, QuerySet
 from django.http import HttpResponse
-from django.utils.html import format_html, urlencode
+from django.utils.html import format_html, urlencode, format_html_join
 from django.urls import reverse
 
 
@@ -58,21 +58,21 @@ class ProductAdmin(admin.ModelAdmin): # With this class we can specify how we wa
 
 # Loading Related Objects
 
-@admin.register(models.Product)
-class ProductAdmin(admin.ModelAdmin): 
-    list_display = ['title', 'unit_price', 'inventory_status','collection_title']
-    list_editable = ['unit_price'] 
-    list_per_page = 100 # pagination
-    list_select_related = ['collection']
+# @admin.register(models.Product)
+# class ProductAdmin(admin.ModelAdmin): 
+#     list_display = ['title', 'unit_price', 'inventory_status','collection_title']
+#     list_editable = ['unit_price'] 
+#     list_per_page = 100 # pagination
+#     list_select_related = ['collection']
 
-    def collection_title(self,product):
-        return product.collection.title
+#     def collection_title(self,product):
+#         return product.collection.title
     
-    @admin.display(ordering = 'inventory')
-    def inventory_status(self, product):
-        if product.inventory < 10:
-            return 'LOW'
-        return "OK"
+#     @admin.display(ordering = 'inventory')
+#     def inventory_status(self, product):
+#         if product.inventory < 10:
+#             return 'LOW'
+#         return "OK"
 
 @admin.register(models.Order)
 class OrderAdmin(admin.ModelAdmin):
@@ -124,14 +124,148 @@ class CollectionAdmin(admin.ModelAdmin):
         )
 
 
-@admin.register(models.Customer)
-class CustomerAdmin(admin.ModelAdmin):
-    list_display = ['first_name', 'last_name', 'membership', 'ordered_id']
-    list_editable = ['membership']
-    # ordering = ['first_name', 'last_name'] 
-    list_per_page = 20
+# @admin.register(models.Customer)
+# class CustomerAdmin(admin.ModelAdmin):
+#     list_display = ['first_name', 'last_name', 'membership', 'total_orders' ,'orders']
+#     list_editable = ['membership']
+#     # ordering = ['first_name', 'last_name'] 
+#     list_per_page = 20
 
-    @admin.display(ordering='first_name')
-    def ordered_id(self, order):
+#     # Total number of Orders
+#     @admin.display(ordering='total_orders',description='Orders')
+#     def total_orders(self, customer):
+#         if customer.total_orders == 0:
+#             return 0
+        
+#         url = (
+#             reverse('admin:store_order_changelist') +
+#             '?' +
+#             urlencode(
+#                 {
+#                     'customer__id' : customer.id
+#                 }
+#             )
+#         )
+
+#         return format_html('<a href="{}"> {}</a>', url, customer.total_orders)
+
+#     # Each Orders ordered by the customer
+#     @admin.display(description = 'Orders')
+#     def orders(self, customer):
+#         ordered = customer.order_set.all()
+
+#         if not ordered:
+#             return '-' 
+#         return format_html_join(
+#         ', ',
+#         '<a href="{}">{}</a>',
+#         (
+#             (
+#                 reverse('admin:store_order_change', args=[order.id]),
+#                 order.id
+#             )
+#             for order in ordered
+#         )
+#     )
+
+#     def get_queryset(self, request:HttpResponse) -> QuerySet:
+#         return (
+#         super()
+#         .get_queryset(request)
+#         .annotate(total_orders = Count('order'))
+#         .prefetch_related('order_set')
+#         )
         
 
+#<------------------------------------------------------------------------------------------------------------------->
+
+# Adding Search to the List Page
+
+@admin.register(models.Customer)
+class CustomerAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'membership', 'total_orders' ,'orders']
+    list_editable = ['membership']
+    ordering = ['first_name', 'last_name'] 
+    list_per_page = 20
+    search_fields = ['first_name__istartswith', 'last_name__istartswith']
+
+    # Total number of Orders
+    @admin.display(ordering='total_orders',description='Orders')
+    def total_orders(self, customer):
+        if customer.total_orders == 0:
+            return 0
+        
+        url = (
+            reverse('admin:store_order_changelist') +
+            '?' +
+            urlencode(
+                {
+                    'customer__id' : customer.id
+                }
+            )
+        )
+
+        return format_html('<a href="{}"> {}</a>', url, customer.total_orders)
+
+    # Each Orders ordered by the customer
+    @admin.display(description = 'Orders')
+    def orders(self, customer):
+        ordered = customer.order_set.all()
+
+        if not ordered:
+            return '-' 
+        return format_html_join(
+        ', ',
+        '<a href="{}">{}</a>',
+        (
+            (
+                reverse('admin:store_order_change', args=[order.id]),
+                order.id
+            )
+            for order in ordered
+        )
+    )
+
+    def get_queryset(self, request:HttpResponse) -> QuerySet:
+        return (
+        super()
+        .get_queryset(request)
+        .annotate(total_orders = Count('order'))
+        .prefetch_related('order_set')
+        )
+
+
+#<------------------------------------------------------------------------------------------------------------------->
+
+# Adding Filtering to the page
+
+# Customizing the filter results
+class InventoryFilter(admin.SimpleListFilter):
+    title = 'inventory'
+    parameter_name = 'inventory'
+
+    def lookups(self, request, model_admin):
+        return [
+            ('<10', 'Low')
+        ]
+    
+    def queryset(self, request, queryset):
+        if self.value() == '<10':
+           return queryset.filter(inventory__lt=10)
+
+@admin.register(models.Product)
+class ProductAdmin(admin.ModelAdmin): 
+    list_display = ['title', 'unit_price', 'inventory_status','collection_title']
+    list_editable = ['unit_price'] 
+    list_filter = ['collection', 'last_update', InventoryFilter]
+    list_per_page = 100 # pagination
+    list_select_related = ['collection']
+
+    def collection_title(self,product):
+        return product.collection.title
+    
+    @admin.display(ordering = 'inventory')
+    def inventory_status(self, product):
+        if product.inventory < 10:
+            return 'LOW'
+        return "OK"
